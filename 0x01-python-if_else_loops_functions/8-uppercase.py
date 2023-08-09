042eca1e016746719d54523f88237d6308f061c3
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-09 11:01:50 -0700

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for c in str:
+        if ord(c) > 96 and ord(c) < 123:
+            c = chr(ord(c) - 32)
+        print("{:s}".format(c), end="")
+    print("")
