71474c4d72015f0f54f60f0ff9e25c156074eeaf
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-10 15:00:35 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(sys.argv)
+
+    if argc == 1:
+        print("0 arguments.")
+    elif argc == 2:
+        print("1 argument:")
+        print("1: {:s}".format(argv[1]))
+    elif argc > 2:
+        print("{:d} arguments:".format(argc - 1))
+        for i in range(1, argc):
+            print("{:d}: {:s}".format(i, argv[i]))
