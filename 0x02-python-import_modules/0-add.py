fd7a0cbd96da206515e169283dea351c2a510e1c
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-10 13:43:50 -0700

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Import the add function from add_0.py
+from add_0 import add
+
+# Define and calculate the result using the imported function
+a = 1
+b = 2
+result = add(a, b)
+
+# Print the result
+print(f"{a} + {b} = {result}")
