e40a448975813483c25709b87ccf705d4a054b01
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-10 15:10:14 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,11 @@ if __name__ == "__main__":
 
     if argc == 1:
         print("0 arguments.")
+
     elif argc == 2:
         print("1 argument:")
         print("1: {:s}".format(argv[1]))
+
     elif argc > 2:
         print("{:d} arguments:".format(argc - 1))
         for i in range(1, argc):
