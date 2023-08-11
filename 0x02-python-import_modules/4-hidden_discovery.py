4b1ae0bec02466b8a7ad62029aff8155a41543fe
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-10 17:24:01 -0700

    task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import hidden_4
+
+def print_names():
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print("{:s}".format(name))
+
+
+if __name__ == "__main__":
+    print_names()
