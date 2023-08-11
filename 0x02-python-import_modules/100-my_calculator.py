934642ee1843a8a6dabb63b537bcaee8ddcd3258
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-11 08:42:19 -0700

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-import sys
 from calculator_1 import add, sub, mul, div
 
 
-def calculate():
-    argv = sys.argv
-    argc = len(sys.argv)
-
-    if argc != 4:
+def calculate(av, ac):
+    if ac != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        return (1)
+        exit(1)
 
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
+    a = int(av[1])
+    op = av[2]
+    b = int(av[3])
 
     if op == '+':
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
@@ -25,7 +21,12 @@ def calculate():
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
 
 
 if __name__ == "__main__":
-    calculate()
+    import sys
+    av = sys.argv
+    ac = len(sys.argv)
+
+    calculate(av, ac)
