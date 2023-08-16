97e5dc33cabad055cbb528f1f4ab241ac8dfc5a0
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-16 15:07:47 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string is None:
+    if isinstance(roman_string, str) or roman_string is None:
         return (0)
 
     roman_numbers = {
-        'I': 1, 'IV': 4, 'V' : 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90,
+        'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90,
         'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000
     }
 
