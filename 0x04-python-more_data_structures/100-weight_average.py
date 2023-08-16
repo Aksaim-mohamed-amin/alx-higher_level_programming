02c8458b3ba8a3eb2f78a833c58a06b66dff54e3
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-16 15:24:30 -0700

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    x = y = 0
-    for i in my_list:
-        x += i[0] * i[1]
-        y += i[1]
-
-    return (x / y)
+    if my_list:
+        x = y = 0
+        for i in my_list:
+            x += i[0] * i[1]
+            y += i[1]
+            return (x / y)
+    return (0)
