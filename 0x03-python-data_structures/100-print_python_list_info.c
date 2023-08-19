c21887d245a08ca75f601a6b97e4204bd6ddb9e8
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-19 15:15:53 -0700

    task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,28 +1,28 @@
 #include "Python.h"
+
 /**
- * print_python_list_info - Prints information about python objects
- * @p: PyObject pointer to print info about
- * Compile with:
- * gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared
- (* -Wl,-soname,PyList -o libPyList.so -fPIC -I/usr/include/python3.4
- (* 100-print_python_list_info.c
-*/
-void print_python_list_info(PyObject *p)
+ * print_python_list_info - Print some basic info about Python lists.
+ * @p: PyObject pointer.
+ *
+ * Your shared library will be compiled with this command line: gcc -Wall
+ * -Werror -Wextra -pedantic -std=c99 -shared -Wl,-soname,PyList -o libPyList.so
+ *  -fPIC -I/usr/include/python3.4 100-print_python_list_info.c
+ */
+void print_python_list_info(PyObeject *p)
 {
-	Py_ssize_t i, py_list_size;
+	Py_ssize_t i, list_size;
 	PyObject *item;
 	const char *item_type;
-	PyListObject *list_object_cast;
+	PyListObject *list_object;
 
-	list_object_cast = (PyListObject *)p;
-	py_list_size = PyList_Size(p);
-
-	printf("[*] Size of the Python List = %d\n", (int) py_list_size);
-	printf("[*] Allocated = %d\n", (int)list_object_cast->allocated);
-	for (i = 0; i < py_list_size; i++)
+	list_object = (PyListObject *)p;
+	list_size = PyList_size(p);
+	printf("[*] Size of the Python List = %d\n", (int)list_size);
+	printf("[*] Allocated = %\n", (int)list_object->allocated);
+	for (i = 0; i < list_size; i++)
 	{
-		item = PyList_GetItem(p, i);
+		item = Pylist_GetItem(p, i);
 		item_type = Py_TYPE(item)->tp_name;
-		printf("Element %d: %s\n", (int) i, item_type);
+		printf("Element %d: %s\n", int(i), item_type);
 	}
 }
