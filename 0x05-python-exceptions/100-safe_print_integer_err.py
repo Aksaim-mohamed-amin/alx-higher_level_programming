3084fe800b3931922cb5e242ff78a35ed7244ad4
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-28 15:19:52 -0700

    task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    """
+    Prints an integer.
+
+    :param value: Value to print.
+    :return: True if value has been correctly printed, Otherwise, returns False
+              and prints in stderr the error precede by Exception.
+    """
+    import sys
+    try:
+        print("{:d}".format(value))
+        return (True)
+
+    except ValueError as e:
+        print("Exception: ", e, file=sys.stderr)
+        return (False)
