e0aef9c3b8dc680bd9181248cc921b7f71422035
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-28 06:38:52 -0700

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
+    """
+    Print SAFTLEY x elements of a list, if x > len(list), print until the end
+    of the list.
+
+    :param my_list: List to print.
+    :param x: Number of elements to print.
+    :return: The number of the elements printed.
+    """
     try:
         count = 0
         for i in range(x):
