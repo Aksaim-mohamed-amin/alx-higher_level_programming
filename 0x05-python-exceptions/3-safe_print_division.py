80ed4763f7d25f96acafb13c9f16e386ab4aa5f9
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-08-28 07:47:43 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -10,9 +10,8 @@ def safe_print_division(a, b):
     try:
         result = a / b
 
-    except:
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
-
-    return (result)
+        return (result)
