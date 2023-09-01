291008f7a8fe87c2370b08195144a36241f37668
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-09-01 14:25:27 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Square Class """
+
+
+class Square:
+    """ Square """
+
+    def __init__(self, size):
+        """ Initialize a new Square.
+
+        Args:
+             size (int): Size of the new Square.
+        """
+        self.__size = size
