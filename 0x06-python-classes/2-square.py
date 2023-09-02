76926018d8f29b8d6d0c7c5ef0b1077b1234a7ab
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-09-02 02:07:17 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Square Class """
+
+
+class Square:
+    """ Square """
+
+    def __init__(self, size=0):
+        """ Initialize a new Square.
+
+        Args:
+             size (int): Size of the new Square.
+        """
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+        self.__size = size
