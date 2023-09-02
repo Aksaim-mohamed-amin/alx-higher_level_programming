7e13866841924cc2f7d42237cea88201221b819c
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-09-02 10:16:47 -0700

    task 10

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -13,15 +13,13 @@ class MagicClass:
         Args:
           radius (float or int): The raduis of the new cyrcle.
         """
-        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        else:
-            self.__radius = radius
+        self.__radius = radius
 
     def area(self):
         """ return thearea of the cyrcle """
-        return (2 ** self.__radius * math.pi)
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
         """ return the circumference of the cycle """
