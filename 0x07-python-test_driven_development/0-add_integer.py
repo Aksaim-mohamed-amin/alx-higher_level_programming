8e1f205f5e30d87d27ed1b0281b66a32ca0303b3
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-09-05 14:10:19 -0700

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """ Add two integers """
+    """ Add two integers
+
+    Args:
+      a (int): First Number.
+      b (int) (optional): Secend Number.
+
+    Return: a + b
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
