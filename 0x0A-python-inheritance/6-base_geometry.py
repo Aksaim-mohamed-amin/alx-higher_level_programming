ab73d51dbae0f48937adcffb4f1f795dc07e3693
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-09-11 06:43:36 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" BaseGeometry class """
+
+
+class BaseGeometry:
+    """ Define a BaseGeometry class """
+
+    def area(self):
+    """ Raise an exception with the area not emplimented """
+        raise Exception("area() is not implemented")
