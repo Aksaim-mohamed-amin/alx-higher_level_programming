cb10514193243cd9e6d63a50c1f96d998291164c
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-09-14 09:14:51 -0700

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Student Class """
+
+
+class Student:
+    """ Define a class student """
+    def __init__(self, first_name, last_name, age):
+        """ Initialize a new instance of student class
+
+        Args:
+          fisr_name: Student fisrt name.
+          last_name: Student last name.
+          age: Student age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Return a dictionary representation of a Student instance
+
+        Args:
+          attrs: list of the attributes to return if None return all.
+        """
+        if attrs is None:
+            return (self.__dict__)
+        return {atr: getattr(self, atr) for atr in attrs if hasattr(self, atr)}
+
+    def reload_from_json(self, json):
+        """ Replace all the attributes of a student
+
+        Args:
+          json (dict): Dictionary that contain all the new attrbutes.
+        """
+        self.__dict__ = json
