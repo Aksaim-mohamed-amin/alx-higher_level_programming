ee0f3ddd3094fc8d6f33641dfac5450792854485
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2023-09-14 09:01:26 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Student Class """
+
+
+class Student:
+    """ Define a class student """
+    def __init__(self, first_name, last_name, age):
+        """ Initialize a new instance of student class
+
+        Args:
+          fisr_name: Student fisrt name.
+          last_name: Student last name.
+          age: Student age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Return a dictionary representation of a Student instance
+
+        Args:
+          attrs: list of the attributes to return if None return all.
+        """
+        if attrs == None:
+            return (self.__dict__)
+        return ({attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)})
