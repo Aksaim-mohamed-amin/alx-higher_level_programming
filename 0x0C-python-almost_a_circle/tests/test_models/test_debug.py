bcbc15f3655bc9d5ae36fc04eb2957fef7af5911
root <aksaimmohamedamin@gmail.com>
2023-09-18 14:47:55 -0700

    tasks

--- 0x0C-python-almost_a_circle/tests/test_models/test_debug.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class TestDebug(unittest.TestCase):
-    def test_debug(self):
-        loader = unittest.TestLoader()
-        suite = loader.discover('tests', pattern='test*.py')
-        for test in suite:
-            print(test)
