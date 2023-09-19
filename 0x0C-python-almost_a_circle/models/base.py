ed03697e7294babe8299db804d309dc2a5c7ed79
root <aksaimmohamedamin@gmail.com>
2023-09-19 14:45:11 -0700

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Base Class """
-
+import json
 
 class Base:
     """ define the base class"""
@@ -17,3 +17,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    # to_json_string
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries.
+
+        Args:
+           list_dictionaries (list of dict): List of dictionary to convert.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ([])
+
+        return (json.dumps(list_dictionaries))
