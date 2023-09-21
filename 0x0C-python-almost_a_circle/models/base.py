62ad795e0462a8163a4fe308dd8356ac1b39e725
root <aksaimmohamedamin@gmail.com>
2023-09-21 08:39:18 -0700

    task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -51,7 +51,7 @@ class Base:
 
     # from json string
     def from_json_string(json_string):
-        """ returns the list of the JSON string representation of json_string"""
+        """returns the list of the JSON string representation of json_string"""
         if json_string is None or len(json_string) == 0:
             return ([])
         return (json.loads(json_string))
