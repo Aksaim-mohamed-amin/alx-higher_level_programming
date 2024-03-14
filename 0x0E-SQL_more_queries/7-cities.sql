0b1c30c364048bf3508675c02cf7f24c9b9a9257
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-14 11:13:51 -0700

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,8 +8,8 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `cities` (
-`id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 `state_id` INT NOT NULL,
 `name` VARCHAR(256) NOT NULL,
-FOREIGN KEY `state_id` REFERENCES `states`(`id`)
+FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
 );
