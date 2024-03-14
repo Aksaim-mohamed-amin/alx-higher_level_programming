cfbed4fb91160142beb84ae5074f7f0e4a31b661
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-14 11:14:29 -0700

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -11,5 +11,5 @@ CREATE TABLE IF NOT EXISTS `cities` (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 `state_id` INT NOT NULL,
 `name` VARCHAR(256) NOT NULL,
-FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
+FOREIGN KEY `state_id` REFERENCES `states`(`id`)
 );
