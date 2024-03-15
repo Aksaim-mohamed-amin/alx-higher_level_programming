256d36684e2d9f7e2a3c68b7430f80a1e14fee9a
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-15 10:18:29 -0700

    task 20

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in descending order by their rating
+-- You can use only one SELECT statement
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_show_ratings
+JOIN tv_show_genres ON tv_show_ratings.show_id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY rating;
