1be6d04753597f1e161c3da9e814fc1b5e574f54
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-15 08:22:51 -0700

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,6 +5,6 @@
 -- You can use only one SELECT statement
 SELECT name
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE name = 'Dexter';
+WHERE title = 'Dexter';
