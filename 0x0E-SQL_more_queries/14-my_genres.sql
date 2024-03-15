5be5c263c8bfda6862aa5547e9806970859dbf12
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-15 08:52:07 -0700

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,4 +7,4 @@ SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE title = 'Dexter';
+WHERE tv_shows.title = 'Dexter';
