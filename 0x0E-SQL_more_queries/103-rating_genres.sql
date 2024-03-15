a30243d3f5533e98af0b6b9d290171752497279f
Aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-03-15 10:19:47 -0700

    task 20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -7,4 +7,4 @@ FROM tv_show_ratings
 JOIN tv_show_genres ON tv_show_ratings.show_id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
-ORDER BY rating;
+ORDER BY rating DESC;
