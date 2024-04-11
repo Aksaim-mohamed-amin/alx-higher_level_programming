cc2f89b044a0dd38f2ab12203cd654449c8c2c48
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-11 05:12:21 -0700

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -10,7 +10,7 @@ class Rectangle {
    * @param {number} h - The height of the rectangle.
    */
   constructor (w, h) {
-    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) return {};
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) return;
     this.width = w;
     this.height = h;
   }
