3ad79a7d2553113d4e701b18ed11136fbad35eca
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-12 14:05:29 -0700

    task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -8,13 +8,7 @@ const fs = require('fs');
 const [, , fileA, fileB, fileC] = process.argv;
 
 fs.readFile(fileA, 'utf8', function (errA, dataA) {
-
-  fs.readFile(fileB, 'utf8', function(errB, dataB) {
-
-    const content = dataA + dataB;
-
-    fs.writeFile(fileC, content, 'utf8', function(errWrite) {
-      
-    });
+  fs.readFile(fileB, 'utf8', function (errB, dataB) {
+    fs.writeFile(fileC, dataA + dataB, 'utf8', function (errWrite) {});
   });
 });
