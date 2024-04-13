32f3d24853b02d34cb0006ab42bf08a077621968
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-13 05:16:34 -0700

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" lists all cities from database """
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to database
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor
+    cursor = db.cursor()
+
+    # Execute the command in SQL
+    cursor.execute("SELECT * FROM cities ORDER BY cities.id")
+
+    # Retrive the result
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    # Close connection
+    cursor.close()
+    db.close()
