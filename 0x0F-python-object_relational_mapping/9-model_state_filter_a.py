98e0c1b7d597b882b9d2426a502fe65f2d247c39
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-14 04:12:24 -0700

    task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -16,6 +16,9 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
+    # Create all tables defined by ORM-mapped classes in the database
+    Base.metadata.create_all(engine)
+
     # Create a sessionmaker object
     Session = sessionmaker(bind=engine)
 
