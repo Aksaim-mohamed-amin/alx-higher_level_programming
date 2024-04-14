afb8c91d1952a997b2cc9aff2f9c93e60dc0b221
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-14 03:27:09 -0700

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+prints the State id with the name passed as argument from the database
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+
+    # Connect to Mysql server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a sessionmaker object
+    Session = sessionmaker(bind=engine)
+
+    # Create a new session
+    session = Session()
+
+    # Query the first state object
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Nothing")
+
+    # Close the session
+    session.close()
