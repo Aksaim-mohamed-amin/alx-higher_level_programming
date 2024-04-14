a823c0d922f7ded3d64200fcb81bcfe29fca1f98
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-14 04:53:23 -0700

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+changes the name of a State object from the database.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import NoResultFound
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+
+    # Connect to Mysql server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create all tables defined by ORM-mapped classes in the database
+    Base.metadata.create_all(engine)
+
+    # Create a sessionmaker object
+    Session = sessionmaker(bind=engine)
+
+    # Create a new session
+    session = Session()
+
+    # Select and Update the state name
+    try:
+        state = session.query(State).filter(State.id == 2).one()
+        state.name = "New Mexico"
+        session.commit()
+
+    except NoResultFound:
+        print("Not Found")
+    
+    # Close the session
+    session.close()
