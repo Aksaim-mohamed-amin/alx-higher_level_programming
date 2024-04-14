2b5036cb3721becbd429f17d645e07f77e461358
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-14 10:05:49 -0700

    task 15

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Define City class and Base instance. """
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+    """ City class represents a table in the database. """
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Define State class and Base instance. """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ State class represents a table in the database. """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City")
