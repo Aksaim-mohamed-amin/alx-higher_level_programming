400b1eb74d39f2a80f36c64dee01b41dfec18c64
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-25 16:39:33 -0700

    task 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI "$1" -o /dev/null -w '{%http_code}'
+curl -sLw '{%http_code}' -o /dev/null "$1"
