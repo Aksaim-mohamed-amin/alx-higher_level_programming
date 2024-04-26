230f65391293bd7bad74e0e7d2bc1a00a8218561
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-25 18:09:52 -0700

    task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
