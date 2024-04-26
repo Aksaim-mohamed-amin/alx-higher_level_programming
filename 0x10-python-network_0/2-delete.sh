314a2234d8609e6d97bacb565c2c58e43a1c2979
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-25 18:05:32 -0700

    task 9

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -s -X DELETE "$1"
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
