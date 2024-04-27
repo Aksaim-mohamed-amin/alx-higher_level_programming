35dceda1c28e15f55f25535283a737298544cea1
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-04-27 10:18:16 -0700

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the body of the response
-(decoded in utf-8).
+takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
 """
 
 import sys
