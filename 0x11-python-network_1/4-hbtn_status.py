3d218334a65e1eb16ba3bfec445d71fde41b5cfa
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-07-05 19:45:51 -0700

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-# fetches https://alx-intranet.hbtn.io/status
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
-# URL to fetch
+
 url = 'https://alx-intranet.hbtn.io/status'
 
-# Fetch the URL
 response = requests.get(url)
 
-# Display the body response
 print("Body response:")
 print(f"\t- type: {type(response.text)}")
 print(f"\t- content: {response.text}")
