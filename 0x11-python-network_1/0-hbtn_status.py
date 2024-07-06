81c202c7769e86a5dc15950bc3c10daa22d31e6c
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-07-06 14:22:12 -0700

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" fetch https://alx-intrant.hbtn.io/status using urllib """
+
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body response:')
+    print('\t- type:', type(html))
+    print('\t- content:', html)
+    print('\t- utf8 content:', html.decode('utf-8'))
