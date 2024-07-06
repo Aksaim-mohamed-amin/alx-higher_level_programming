27d7a1f54985e04f099de0cad803d7d4972e7278
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-07-05 19:09:13 -0700

    task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send post request with a json file.
-curl -s -X POST -H "Content-Type: application/json" -d "$2" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
