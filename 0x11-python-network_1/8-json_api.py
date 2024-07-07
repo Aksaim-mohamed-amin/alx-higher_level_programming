62a3182fb6829d9efb0445ddab11723cadb8d5bd
Aksaim Mohamed Amin <aksaimmohamedamin@gmail.com>
2024-07-07 09:11:43 -0700

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+ with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+
+    payload = {'q': letter}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    try:
+        json_data = r.json()
+        if json_data:
+            print(f"[{json_data['id']}] {json_data['name']}")
+        else:
+            print('No result')
+    except requests.exceptions.JSONDecodeError as e:
+        print('Not a valid JSON')
