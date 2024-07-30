968ae9c8eb8342fc77a624becc4491f60069f7ec
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-07-30 11:10:10 -0700

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,15 +2,11 @@
 /* Comput the factorial of a number */
 
 function factorial (num) {
-  if (num === 0) {
+  if (num === 0 || isNaN(num)) {
     return 1;
   } else {
     return num * factorial(num - 1);
   }
 }
 
-if (isNaN(parseInt(process.argv[2]))) {
-  console.log(1);
-} else {
-  console.log(factorial(process.argv[2]));
-}
+console.log(factorial(process.argv[2]));
