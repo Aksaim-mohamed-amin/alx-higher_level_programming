1a35e97738e8b4357682769324b4ff28f4b03563
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-07-30 11:47:27 -0700

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.slice(2).map(e => parseInt(e)).sort((a, b) => b - a);
-  console.log(list[1]);
+  const integers = process.argv.slice(2).map(e => parseInt(e)).sort((a, b) => b - a);
+  const uniqueIntegers = [...new Set(integers)];
+  console.log(uniqueIntegers[1]);
 }
