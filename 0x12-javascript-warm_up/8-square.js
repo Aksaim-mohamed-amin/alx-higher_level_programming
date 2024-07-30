f05f6848df43aca8004384ce5f4e64858f536d01
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-07-30 10:44:32 -0700

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* Print a square */
+const size = parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('x'.repeat(size));
+  }
+}
