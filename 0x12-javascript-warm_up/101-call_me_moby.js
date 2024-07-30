cce1e44f14cb5852bd2e0471f365e0bc56028c98
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-07-30 12:29:36 -0700

    task 15

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* Executes x times a function. */
-exports.callMeMoby = function (n, f) {
-  for (let i = 0; i < n; i++) {
-    f();
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
   }
 };
