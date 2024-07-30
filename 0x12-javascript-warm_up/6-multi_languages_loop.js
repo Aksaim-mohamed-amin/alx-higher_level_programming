83ecd59ccf482d6c31f52885b310276256214db7
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-07-30 10:33:31 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* Prints 3 lines */
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
