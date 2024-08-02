507ae58a2c75f54d92e65b1896412d192cb6497b
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-08-02 06:09:50 -0700

    task 9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* prints the number of arguments already printed and the new argument value. */
+
+const logList = []
+
+exports.logMe = function (item) {
+  const nb = logList.push(item);
+  console.log(nb - 1 + ': ' + item);
+};
