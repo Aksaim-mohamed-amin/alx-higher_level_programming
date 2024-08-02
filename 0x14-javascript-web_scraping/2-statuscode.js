466e7090ba05b5ead981e89910241bb57da4ef82
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-08-02 15:20:17 -0700

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Display the status code of a GET request. */
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, function (err, rs, body) {
+  if (err) {
+    console.log('Error:', err);
+    return;
+  } else {
+    console.log('code:', rs.statusCode);
+  }
+});
