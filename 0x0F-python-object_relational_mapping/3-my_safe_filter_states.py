37d51a74fea2bc0eb3534a5644b5884132082f49
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-03 03:30:22 -0700

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Connect to hbtn_0e_0_usa database and list all states that match the name giving
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    connection = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=db_name,
+        port=3306
+    )
+
+    cur = connection.cursor()
+    cur.execute("""
+    SELECT * FROM states
+    WHERE name LIKE BINARY %s
+    ORDER BY id ASC
+    """, (state_name,))
+
+    states = cur.fetchall()
+    for row in states:
+        print(row)
+
+    cur.close()
+    connection.close()
