8d18e43b5a01b71a492e86462b3294d13d65badf
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-03 03:18:58 -0700

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Connect to hbtn_0e_0_usa database and list all states that starts with letter N
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    connection = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=db_name,
+        port=3306
+    )
+
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC"
+                .format(state_name))
+
+    states = cur.fetchall()
+    for row in states:
+        print(row)
+
+    cur.close()
+    connection.close()
