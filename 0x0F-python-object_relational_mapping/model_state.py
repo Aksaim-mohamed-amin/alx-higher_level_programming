0cfed4f92f01fe4d6dda101dfb778370d4355c62
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-03 10:45:56 -0700

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a State class and an instance Base for SQLAlchemy ORM"""
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+class State(Base):
+    """Represents a state for SQLAlchemy ORM"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
