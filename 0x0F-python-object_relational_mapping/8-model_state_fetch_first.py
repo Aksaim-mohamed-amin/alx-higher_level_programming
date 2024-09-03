866216aea079091e0a881feb9ae21e60b552552a
aksaim-mohamed-amin <aksaimmohamedamin@gmail.com>
2024-09-03 13:05:17 -0700

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -23,5 +23,7 @@ if __name__ == '__main__':
     state = session.query(State).order_by(State.id).first()
     if state:
         print(f"{state.id}: {state.name}")
+    else:
+        print('')
 
     session.close()
